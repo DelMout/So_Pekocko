4fbcd77b1185db1708b17b944632441d16562e41
Delphine Moutault <dlphn@hotmail.fr>
2020-12-03 21:01:07 +0100

    Empecher saisie '$' et '.' pour email et password.

--- backend/Controllers/user.js
+++ backend/Controllers/user.js
@@ -3,21 +3,32 @@ const bcrypt = require('bcrypt');
 const User = require('../Models/User');
 const jwt = require('jsonwebtoken');
 const sha1 = require('sha1');
+const fs = require('fs');
 //const validator = require('node-email-validation');
 const search = require('regex-collection'); // Pour valider l'adresse email
 const passwordValidator = require('password-validator');     // Pour exiger mot de passe fort
 const profilPassword = new passwordValidator();
 
+
 // Propriétés du password
 profilPassword
-    .is().min(8)            // longueur mini=8
+    .is().min(10)            // longueur mini=10
     .is().max(50)            // longueur maxi=50
     .has().uppercase()            // Majuscules imposées
     .has().lowercase()            // Minuscules imposées
     .has().digits()            // Nombres imposés
-    .has().not().spaces()            // Pas d'espaces
-   /* .has().not(/$/);  */             // Pas de $
+    /*.has().not().spaces()    */        // Pas d'espaces
+    .has().not(/\$|=|'|\./)           // Non autorisés : =  '   $ .
+.is().not().oneOf(['toto'])             // N'appartenant pas aux mots de passe les + standards
 
+// Password les + utilisés
+//fs.readFileSync('./password5000.txt', function (err, data) {
+//    if (err) throw err;
+//    var array = data.toString().split("\n");
+//    for (let i in array) {
+//        console.log("Les famous password : " + array[0] + " , " + array[1]);
+//    }
+//});
 
 
 
@@ -25,17 +36,20 @@ profilPassword
 //cryptage du password et sauvegarde dans BdD
 exports.signup = (req, res, next) => {
     console.log("password :" + profilPassword.validate(req.body.password));
-    console.log("password :" + profilPassword.validate(req.body.password, { list: true }));
+    console.log("Erreurs dans le password :" + profilPassword.validate(req.body.password, { list: true }));
     if (!profilPassword.validate(req.body.password)) {
         return res.status(401).json({ error: 'Mot de passe pas assez fort !' });
         console.log("Mot de passe pas assez fort !");
     }
     bcrypt.hash(req.body.password, 10)
         .then(hash => {
-            console.log("email : "+search.isEmailAddress(req.body.email));
-            if (!search.isEmailAddress(req.body.email)) {
+            console.log("email : " + search.isEmailAddress(req.body.email));
+            console.log("position $ : " + req.body.email.indexOf("$"));
+            if (!search.isEmailAddress(req.body.email) || req.body.email.indexOf("$")===0) {   // Commence par '.' est exclu de regex-collection. indexOg exclu le '$' en 1re place mais pas '$'.
             //if (!validator.is_email_valid(req.body.email)){
+                console.log("email pas bon !");
                 return res.status(401).json({ error: 'Email non valide !' });
+                
             }
             const user = new User({
                 email: sha1(req.body.email),
